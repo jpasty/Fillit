49d7d9de9a39bde0419bff7e971744e7b769801a
jpasty <49813695+jpasty@users.noreply.github.com>
2019-08-22 18:48:36 +0300

    Add files via upload
    that's cool

--- includes/fillit.h
+++ includes/fillit.h
@@ -6,7 +6,7 @@
 /*   By: jpasty <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/15 20:12:04 by jpasty            #+#    #+#             */
-/*   Updated: 2019/06/21 13:25:34 by jpasty           ###   ########.fr       */
+/*   Updated: 2019/08/22 18:41:22 by jpasty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,30 +20,28 @@
 
 # define BYTEREAD 21
 # define BLOCK '#'
-# define SPACE '.'
+# define DOT '.'
 
-typedef struct	    s_map
+typedef struct		s_pos
 {
-	char		    *line;
-	int             nbr_of_block;
-
-}				    t_map;
-
-typedef struct      s_pos
-{
-    int             x;
-    int             y;
-}                   t_pos;
+	int				x;
+	int				y;
+}					t_pos;
 
 typedef struct		s_etris
 {
-    t_pos	 		point[4];
+	t_pos			pos[4];
 	struct s_etris	*next;
 }					t_etris;
 
-void				fillopen(int , char **);
-int     			valid_or_not(int);
-t_etris				*initialize(void);
-void				make_termino_list(t_etris **, char *);
+t_etris				*valid_or_not(int fd, t_etris *head);
+int					make_tetrimino_list(t_etris **figure, char *str);
+int					min_board_side(t_etris *figure);
+char				**gen_map(char **map, int side);
+int					just_do_it(char **map, t_etris *figure, char c);
+void				print_map(char **map, t_etris *figure, int fd);
+void				delete_and_exit(t_etris *head, int fd);
+void				ft_free(char **map);
+void				delete_list(t_etris *head);
 
 #endif
